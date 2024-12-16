c2c10759d49dbcd7a2acdb8b4e77ff43013e194b
strubium <113206902+strubium@users.noreply.github.com>
2024-12-15 18:48:40 -0600

    The great fuck-up

--- src/main/java/mcjty/theoneprobe/items/ModItems.java
+++ src/main/java/mcjty/theoneprobe/items/ModItems.java
@@ -115,13 +115,11 @@ public class ModItems {
         ModelLoader.setCustomModelResourceLocation(helmet, 0, new ModelResourceLocation(helmet.getRegistryName(), "inventory"));
     }
 
-    @SideOnly(Side.CLIENT)
     @Deprecated //Old Hook, dont use
     public static boolean isProbeInHand(ItemStack stack) {
         return isProbe(stack);
     }
 
-    @SideOnly(Side.CLIENT)
     public static boolean isProbe(ItemStack stack) {
         if (stack.isEmpty()) {
             return false;
@@ -135,7 +133,6 @@ public class ModItems {
         return stack.getTagCompound().hasKey(PROBETAG);
     }
 
-    @SideOnly(Side.CLIENT)
     private static boolean isProbeHelmet(ItemStack stack) {
         if (stack.isEmpty()) {
             return false;
@@ -149,7 +146,6 @@ public class ModItems {
         return stack.getTagCompound().hasKey(PROBETAG);
     }
 
-    @SideOnly(Side.CLIENT)
     public static boolean hasAProbeSomewhere(EntityPlayer player) {
         return isProbe(player.getHeldItem(EnumHand.MAIN_HAND))
                 || isProbe(player.getHeldItem(EnumHand.OFF_HAND))
