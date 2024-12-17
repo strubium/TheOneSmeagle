56dbe1c5ca5d5143f75f05a33ccaf07ab06e0eaa
strubium <113206902+strubium@users.noreply.github.com>
2024-12-17 10:19:35 -0600

    Ok

--- src/main/java/mcjty/theoneprobe/commands/CommandTopCfg.java
+++ src/main/java/mcjty/theoneprobe/commands/CommandTopCfg.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-@SideOnly(Side.CLIENT)
 public class CommandTopCfg extends CommandBase {
 
     @Override
--- src/main/java/mcjty/theoneprobe/commands/CommandTopNeed.java
+++ src/main/java/mcjty/theoneprobe/commands/CommandTopNeed.java
@@ -13,7 +13,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class CommandTopNeed extends CommandBase {
 
 
